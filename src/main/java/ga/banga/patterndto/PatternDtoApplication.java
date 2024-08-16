ab44d1941f3f17e7ddbb47757bf977a3133ef2a9
BANGA Romaric <romaric.banga@aninf.ga>
2024-08-17 00:04:48 +0100

    Initial commit

---
+++ src/main/java/ga/banga/patterndto/PatternDtoApplication.java
@@ -0,0 +1,13 @@
+package ga.banga.patterndto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ga/banga/patterndto/PatternDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.banga.patterndto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
